a0bb3e3f88eb25ae63bd5e0caed95434d34d1fb4
zishanj <jashsoftpk@aim.com>
2017-02-27 11:45:13 +0500

    Update time-calc.js

--- js/time-calc.js
+++ js/time-calc.js
@@ -44,6 +44,7 @@
             timeCalc.location = "lahore, pakistan";
             timeCalc.timeZone = "+5";
             timeCalc.method = "Karachi";
+            timeCalc.fiqh = "Hanafi";
             
             timeCalc.change = function () {
                 $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + timeCalc.location + '&key=AIzaSyCeYHbsA8mz5ll_2za73-nrvqAd2VaHvXU')
@@ -51,7 +52,7 @@
                     timeCalc.geoData = response.data;
                     
                     prayTimes.setMethod(timeCalc.method);
-                    prayTimes.adjust({asr: 'Hanafi'});
+                    prayTimes.adjust({asr: timeCalc.fiqh});
                     timeCalc.lat = timeCalc.geoData.results[0].geometry.location.lat;
                     timeCalc.lng = timeCalc.geoData.results[0].geometry.location.lng;
                     timeCalc.prayerTimes = prayTimes.getTimes(new Date(), [timeCalc.lat, timeCalc.lng], timeCalc.timeZone);
@@ -65,7 +66,6 @@
                     timeCalc.timeSunset = timeCalc.prayerTimes.sunset;
                     timeCalc.timeMaghrib = timeCalc.prayerTimes.maghrib;
                     timeCalc.timeIsha = timeCalc.prayerTimes.isha;
-
 
                     var dateSunrise = new Date(0, 0, 0, timeCalc.timeSunrise.split(":")[0], timeCalc.timeSunrise.split(":")[1], 0);
                     var dateSunset = new Date(0, 0, 0, timeCalc.timeSunset.split(":")[0], timeCalc.timeSunset.split(":")[1], 0);
